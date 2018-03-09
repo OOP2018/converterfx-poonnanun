56a81d766dcb168050a1127d863c9426e9b94f86
poonnanun <poonnanun1@gmail.com>
2018-03-09 16:43:15 +0700

    Change way to separate field

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -2,9 +2,11 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 
 /**
  * Class that use for controll the application event action.
@@ -31,85 +33,74 @@ public class SampleController {
 	@FXML
 	private ComboBox<Length> to;
 	
+	private static int check = 1;
+	private static Alert error = new Alert(Alert.AlertType.WARNING);
+	
 	/**
 	 * When start program add item to combo box.
+	 * and set the enter pressed to run converter.
 	 */
 	@FXML 
 	public void initialize(){
 		if(from != null && to != null){
 			from.getItems().addAll(Length.values());
+			from.getSelectionModel().select(0);
 			to.getItems().addAll(Length.values());
+			to.getSelectionModel().select(0);
 		}
+		before.setOnKeyPressed(event -> {
+			check = 1;
+			if (event.getCode().equals(KeyCode.ENTER)){
+				System.out.print(1);
+				convert.fire();
+			}
+		});
+		after.setOnKeyPressed(event -> {
+			check = 2;
+			if (event.getCode().equals(KeyCode.ENTER)){
+				System.out.print(2);
+				convert.fire();
+			}
+		});
+		
 	}
 	
 	/**
-	 * Event that use for convert length
+	 * go to methos form last edit field by check local attribute
 	 * @param e
 	 */
 	@FXML
-	public void handleConvert(ActionEvent e){
+	public void converterClick(ActionEvent e){
+		System.out.print(check);
+		if(check == 1) handleConvertLR();
+		if(check == 2) handleConvertRL();
+	}
+	/**
+	 * Event that use for convert length from left to right
+	 * @param e
+	 */
+	public void handleConvertLR(){
 		
 		if(before.getText().equals("")) {
-			//Check in case convert from b to a
-			if(!after.getText().equals("")){
-				
-				//Start case b to a
-				double beforeNumber = Double.parseDouble(after.getText().trim());
-				double afterNumber = 0;
-				
-				//Check if combo box is null
-				if(from.getValue() == null || to.getValue() == null){
-					before.setText("Select type!!!");
-					after.setText("Select type!!!"); 
-					return;
-				}
-				
-				//Get the convertor type in the reverse order 
-				String to = this.from.getValue().toString().trim();
-				String from = this.to.getValue().toString().trim();
-				
-				//get the value for convertor
-				double multiplyNum = -1;
-				double divideNum = -1;
-				Length[] lengths = Length.values();
-				for(Length a: lengths){
-					if(from.equals(a.toString())){
-						multiplyNum = a.getValue();
-					}
-					if(to.equals(a.toString())){
-						divideNum = a.getValue();
-					}
-				}
-				
-				//Calculate in formular
-				if(multiplyNum != -1 || divideNum != 1){
-					afterNumber = (beforeNumber * multiplyNum) / divideNum;
-				}
-				//Check if answer is not null
-				if(afterNumber != 0){
-					before.setText(String.valueOf(afterNumber));
-					return;
-				}
-				//In case that something wrong
-				else{
-					before.setText("Error!!!");
-					after.setText("Error!!!");
-					return;
-				}
-			}
 			//In case that both are null
-			before.setText("Enter the value!"); 
-			after.setText("Enter the value!"); 
+			error.setHeaderText("Input your value");
+			error.showAndWait();
 			return;
 		}
 		//Start case a to b
 		double beforeNumber = Double.parseDouble(before.getText().trim());
 		double afterNumber = 0;
 		
+		//Check if length is lower than zero
+		if(beforeNumber < 0){
+			error.setHeaderText("Length should not be lower than zero");
+			error.showAndWait();
+			return;
+		}
+		
 		//Check if combo box is null
 		if(from.getValue() == null || to.getValue() == null){
-			before.setText("Select type!!!");
-			after.setText("Select type!!!"); 
+			error.setHeaderText("Select the convert type!!");
 			return;
 		}
 		
@@ -142,11 +133,72 @@ public class SampleController {
 		
 		//in case that something went wrong
 		else{
-			before.setText("Error!!!");
-			after.setText("Error!!!");
+			error.setHeaderText("Error!!!");
+			error.showAndWait();
 		}
 	}
 	
+
+	/**
+	 * Event that use for convert length from right to left
+	 * @param e
+	 */
+	public void handleConvertRL(){
+		if(!after.getText().equals("")){
+			
+			//Start case b to a
+			double beforeNumber = Double.parseDouble(after.getText().trim());
+			double afterNumber = 0;
+			
+			//Check if length is lower than zero
+			if(beforeNumber < 0){
+				error.setHeaderText("Length should not be lower than zero");
+				error.showAndWait();
+				return;
+			}
+			
+			//Check if combo box is null
+			if(from.getValue() == null || to.getValue() == null){
+				error.setHeaderText("Select the convert type!!");
+				return;
+			}
+			
+			//Get the convertor type in the reverse order 
+			String to = this.from.getValue().toString().trim();
+			String from = this.to.getValue().toString().trim();
+			
+			//get the value for convertor
+			double multiplyNum = -1;
+			double divideNum = -1;
+			Length[] lengths = Length.values();
+			for(Length a: lengths){
+				if(from.equals(a.toString())){
+					multiplyNum = a.getValue();
+				}
+				if(to.equals(a.toString())){
+					divideNum = a.getValue();
+				}
+			}
+			
+			//Calculate in formular
+			if(multiplyNum != -1 || divideNum != 1){
+				afterNumber = (beforeNumber * multiplyNum) / divideNum;
+			}
+			//Check if answer is not null
+			if(afterNumber != 0){
+				before.setText(String.valueOf(afterNumber));
+				return;
+			}
+			//In case that something wrong
+			else{
+				error.setHeaderText("Error!!!");
+				error.showAndWait();
+				return;
+			}
+		}
+		error.setHeaderText("Input your value");
+		error.showAndWait();
+	}
 	/**
 	 * Event that clear every thing in textField
 	 * @param e
